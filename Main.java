a2ed14a951d22960e5b76af7513b41915b4ad903
Nicholas Wiest <nicwiest@gmail.com>
2018-08-27 12:47:06 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
+//ASCII Art
+//Nicholas Wiest CMPSC 221
+
 public class Main 
 {
   
    public static void main(String[] args) 
    {
-      System.out.println("  *  ");
-      System.out.println(" *** ");
-      System.out.println("*****");
+      System.out.println(".---------. | == |");
+      System.out.println("|.-------.| |----|");
+      System.out.println("||       || | == |");
+      System.out.println("||       || |----|");
+      System.out.println("|'-.....-'| |::::|");
+      System.out.println("   )---(`---|___.|");
+      System.out.println("/:::::::::::\" _  ");
   }
   
 }
